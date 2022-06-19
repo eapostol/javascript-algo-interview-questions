e56c6f7865a054b0f96583e84eeb6b7f771e78dd
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-20 00:10:45 +0800

    Time: 186 ms (25.99%), Space: 51.4 MB (27.93%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let numFrequency = {};
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (!(num in numFrequency)) {
+            numFrequency[num] = 0;
+        }
+        numFrequency[num] += 1;
+        
+        if (numFrequency[num] > 1) {
+            return true;
+        }
+    }
+    return false;
+};
