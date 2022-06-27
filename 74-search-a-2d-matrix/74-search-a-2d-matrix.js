280a303bcb57e7d1b5bde6bef7552b8829256897
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-28 01:22:05 +0800

    Time: 76 ms (66.94%), Space: 41.9 MB (79.17%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -6,56 +6,19 @@
 var searchMatrix = function(matrix, target) {
     const m = matrix.length;
     const n = matrix[0].length;
-    const flatArray = flattenArray(matrix);
     
-//     let row = 0,
-//         col = n - 1;
+    let row = 0,
+        col = n - 1;
     
-//     while (row < m && col > -1) {
-//         const current = matrix[row][col];
-//         if (current === target) {
-//             return true;
-//         } else if (current > target) {
-//             col -= 1;
-//         } else if (current < target) {
-//             row += 1;
-//         }
-//     }
-//     return false;
-    
-    const found = binarySearch(flatArray, target);
-    if (found) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-function binarySearch(arr, target) {
-    let start = 0,
-        end = arr.length;
-    
-    while (start <= end) {
-        const middle = Math.floor(start + (end - start) / 2);
-        
-        if (target == arr[middle]) {
+    while (row < m && col > -1) {
+        const current = matrix[row][col];
+        if (current === target) {
             return true;
-        } else if (target > arr[middle]) {
-            start = middle + 1;
-        } else {
-            end = middle - 1;
+        } else if (current > target) {
+            col -= 1;
+        } else if (current < target) {
+            row += 1;
         }
     }
     return false;
-}
-
-function flattenArray(arr, flattenedArr = []) {
-    for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[i])) {
-            flattenArray(arr[i], flattenedArr);
-        } else {
-            flattenedArr.push(arr[i]);
-        }
-    }
-    return flattenedArr;
-}
+};
