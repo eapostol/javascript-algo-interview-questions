c6afe3777ef89883802aa78bd507261b7e8715ce
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-28 03:33:41 +0800

    Time: 217 ms (12.56%), Space: 45.5 MB (59.12%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -6,19 +6,23 @@ var firstUniqChar = function(s) {
     if (s.length == 1) {
         return 0;
     }
-    let charFrequency = {};
+    const n = s.length;
+    let charFrequency = {},
+        left = 0;
     
-    for (let i = 0; i < s.length; i++) {
-        if (!(s[i] in charFrequency)) {
-            charFrequency[s[i]] = 0;
+    for (let right = 0; right < n; right++) {
+        if (!(s[right] in charFrequency)) {
+            charFrequency[s[right]] = 0;
         }
-        charFrequency[s[i]] += 1;
-    }
-    
-    for (let i = 0; i < s.length; i++) {
-        if (charFrequency[s[i]] == 1) {
-            return i;
+        charFrequency[s[right]] += 1;
+        if (charFrequency[s[right]] > 1) {
+            while (left < n && charFrequency[s[left]] > 1) {
+                left += 1;
+            }
+            if (left === n) {
+                return -1;
+            }
         }
     }
-    return -1;
+    return left;
 };
