45dfb64e2edf17b69d214c3a669c18810c7bcb25
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-28 00:52:54 +0800

    Time: 71 ms (76.70%), Space: 42.9 MB (12.45%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const flatArray = flattenArray(matrix);
+//     let row = 0,
+//         col = n - 1;
+    
+//     while (row < m && col > -1) {
+//         const current = matrix[row][col];
+//         if (current === target) {
+//             return true;
+//         } else if (current > target) {
+//             col -= 1;
+//         } else if (current < target) {
+//             row += 1;
+//         }
+//     }
+//     return false;
+    
+    const found = binarySearch(flatArray, target, 0, flatArray.length);
+    if (found) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function binarySearch(arr, target, start = 0, end = 0) {
+    
+    while (start <= end) {
+        const middle = Math.floor(start + (end - start) / 2);
+        
+        if (target == arr[middle]) {
+            return true;
+        } else if (target > arr[middle]) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+    return false;
+}
+
+function flattenArray(arr, flattenedArr = []) {
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'object') {
+            flattenArray(arr[i], flattenedArr);
+        } else {
+            flattenedArr.push(arr[i]);
+        }
+    }
+    return flattenedArr;
+}
