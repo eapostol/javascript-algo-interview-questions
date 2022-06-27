68600e7fcb5f10e7b05f09d1ab2a825d30a92def
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-28 02:30:09 +0800

    Time: 186 ms (23.96%), Space: 45.9 MB (40.63%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    if (s.length == 1) {
+        return 0;
+    }
+    let charFrequency = {};
+    
+    for (let i = 0; i < s.length; i++) {
+        if (!(s[i] in charFrequency)) {
+            charFrequency[s[i]] = 0;
+        }
+        charFrequency[s[i]] += 1;
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (charFrequency[s[i]] == 1 && s[i] in charFrequency) {
+            return i;
+        }
+    }
+    return -1;
+};
