03e4aa7d160a33f028728ef44e24067b9d58cf9c
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-06-28 04:06:41 +0800

    Time: 132 ms (46.03%), Space: 43.5 MB (92.60%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -4,24 +4,34 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    let charFrequency = {};
+    let charFrequency = new Map();
     
     for (let i = 0; i < magazine.length; i++) {
-        if (!(magazine[i] in charFrequency)) {
-            charFrequency[magazine[i]] = 0;
-        }
-        charFrequency[magazine[i]] += 1;
+        const val = magazine[i];
+        let count = charFrequency.has(val) ? charFrequency.get(val) : 0;
+        charFrequency.set(val,  count + 1);
+        // if (!(magazine[i] in charFrequency)) {
+            // charFrequency[magazine[i]] = 0;
+        // }
+        // charFrequency[magazine[i]] += 1;
     }
     
     for (let j = 0; j < ransomNote.length; j++) {
-        if (ransomNote[j] in charFrequency) {
-            charFrequency[ransomNote[j]] -= 1;
-            if (charFrequency[ransomNote[j]] === 0) {
-                delete charFrequency[ransomNote[j]];
-            }
-        } else {
+        const val = ransomNote[j];
+        if (!charFrequency.has(val) || charFrequency.get(val) == 0) {
             return false;
+        } else {
+            let count = charFrequency.get(val) ? charFrequency.get(val) : 0;
+            charFrequency.set(val,  count - 1);
         }
+    //     if (ransomNote[j] in charFrequency) {
+    //         charFrequency[ransomNote[j]] -= 1;
+    //         if (charFrequency[ransomNote[j]] === 0) {
+    //             delete charFrequency[ransomNote[j]];
+    //         }
+    //     } else {
+    //         return false;
+    //     }
     }
     return true;
 };
