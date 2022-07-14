947c000a4aa2b9f72dcad15e4e5cd8ddb8f37767
Zhafri Shafiq <zhafrishafiq@gmail.com>
2022-07-14 11:07:04 +0800

    Time: 91 ms (64.88%), Space: 44.1 MB (69.65%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -18,13 +18,7 @@ var mergeTwoLists = function(list1, list2) {
         return list1;
     }
     
-    let head = list1;
-    if (list1.val > list2.val) {
-        head = list2;
-        list2 = list2.next;
-    } else {
-        list1 = list1.next;
-    }
+    let head = new ListNode();
     let tail = head;
     
     while (list1 && list2) {
@@ -44,5 +38,5 @@ var mergeTwoLists = function(list1, list2) {
         tail.next = list1;
     }
     
-    return head;
+    return head.next;
 };
